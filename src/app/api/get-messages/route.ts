9d0bd57c4edd0353c1890dc6e36213997b2373ad
Aryan Kumar <aryankumar877@gmail.com>
2024-01-04 13:43:17 +0530

    profile route changed to u

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -1,9 +1,9 @@
-import UserModel from '@/model/User';
-import { getServerSession } from 'next-auth/next';
 import dbConnect from '@/lib/dbConnect';
-import { authOptions } from '../auth/[...nextauth]/options';
-import { User } from 'next-auth';
+import UserModel from '@/model/User';
 import mongoose from 'mongoose';
+import { User } from 'next-auth';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../auth/[...nextauth]/options';
 
 export async function GET(request: Request) {
   await dbConnect();
@@ -11,8 +11,8 @@ export async function GET(request: Request) {
   const _user: User = session?.user;
 
   if (!session || !_user) {
-    return new Response(
-      JSON.stringify({ success: false, message: 'Not authenticated' }),
+    return Response.json(
+      { success: false, message: 'Not authenticated' },
       { status: 401 }
     );
   }
@@ -21,24 +21,27 @@ export async function GET(request: Request) {
     const user = await UserModel.aggregate([
       { $match: { _id: userId } },
       { $unwind: '$messages' },
-      { $sort: { 'messages.createdAt': -1 } }, 
+      { $sort: { 'messages.createdAt': -1 } },
       { $group: { _id: '$_id', messages: { $push: '$messages' } } },
     ]).exec();
 
     if (!user || user.length === 0) {
-      return new Response(
-        JSON.stringify({ message: 'User not found', success: false }),
+      return Response.json(
+        { message: 'User not found', success: false },
         { status: 404 }
       );
     }
 
-    return new Response(JSON.stringify({ messages: user[0].messages }), {
-      status: 200,
-    });
+    return Response.json(
+      { messages: user[0].messages },
+      {
+        status: 200,
+      }
+    );
   } catch (error) {
     console.error('An unexpected error occurred:', error);
-    return new Response(
-      JSON.stringify({ message: 'Internal server error', success: false }),
+    return Response.json(
+      { message: 'Internal server error', success: false },
       { status: 500 }
     );
   }
